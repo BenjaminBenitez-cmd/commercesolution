00695fd8e55c11b15647c8a22729d9169b87ce01
BenjaminBenitez-cmd <benitezbenjamin853@gmail.com>
2021-01-20 20:51:49 -0600

    ready for deployment

---
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -0,0 +1,111 @@
+import React, {useEffect, useState} from 'react';
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button, CssBaseline } from '@material-ui/core';
+
+import useStyles from './styles';
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
+import { commerce } from '../../../lib/commerce';
+import { Link, useHistory } from 'react-router-dom';
+
+
+
+const steps = ['Shipping address', 'Payment details'];
+
+const Checkout = ( { cart, order, onCaptureCheckout, error } ) => {
+    const [activeStep, setActiveStep] = useState(0);
+    const [checkoutToken, setCheckoutToken] = useState(null);
+    const [isFinished, setIsFinished] = useState(false);
+    const [shippingData, setShippingData ] = useState([]);
+    const classes = useStyles();
+    const history = useHistory();
+
+    const Form = () => activeStep === 0
+        ? <AddressForm checkoutToken={checkoutToken} next={next}/>
+        : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} onCaptureCheckout={onCaptureCheckout} timeout={timeout}/>
+
+    const timeout = () => {
+        setTimeout(() => {
+            setIsFinished(true);
+        }, 3000)
+    }    
+    
+    const Confirmation = () => order.customer ? (
+        <>
+            <div>
+                <Typography variant="h5">Thank you for your purchase, {order.customer.firstname} {order.customer.lastname}</Typography>
+                <Divider className={classes.divider} />
+                <Typography variant="subtitle2">Order ref: {order.customer_reference}</Typography>
+            </div>
+            <br />
+            <Button component={Link} to='/' variant="outlined" type="button"></Button>
+        </>
+    ) : isFinished ? (
+        <>
+            <div>
+                <Typography variant="h5">Thank you for your purchase</Typography>
+                <Divider className={classes.divider} />
+            </div>
+            <br />
+            <Button component={Link} to='/' variant="outlined" type="button"></Button>
+        </>
+    ) : (
+        <div className={classes.spinner}>
+            <CircularProgress />
+        </div>
+    )
+
+
+    if(error){
+        <>
+        <Typography variant="h5">Error: {error}</Typography>
+        <br />
+        <Button component={Link} to='/' variant="outlined" type="button"></Button>
+        </>
+    }
+
+    const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+    const next = (data) => {
+        setShippingData(data);
+
+        nextStep();
+    }
+
+
+    useEffect(() => {
+        const generateToken = async () => {
+            try {
+                const token = await commerce.checkout.generateToken(cart.id, { type: 'cart'});
+                console.log(token);
+                setCheckoutToken(token);
+            } catch (error){
+                history.push('/');
+            }
+        }
+
+        generateToken();
+    }, [cart])
+
+    return (
+        <>
+            <CssBaseline />
+            <div className={classes.toolbar} />
+            <main className={classes.layout}>
+                <Paper className={classes.paper}>
+                    <Typography vatiant="h4" align="center">Checkout</Typography>
+                    <Stepper activeStep={activeStep} className={classes.Stepper}>
+                        {steps.map((step) => (
+                            <Step key={step}>
+                                <StepLabel>{step}</StepLabel>
+                            </Step>
+                        ))}
+                    </Stepper>
+                    {activeStep === steps.length ? <Confirmation /> : checkoutToken && <Form />}
+                </Paper> 
+            </main>
+        </>
+    )
+}
+
+export default Checkout;
